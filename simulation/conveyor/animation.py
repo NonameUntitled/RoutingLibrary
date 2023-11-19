11dc571090e99282d6f0d5892b735adac948fb9b
sashasashasasha151 <sashasashasasha151@gmail.com>
2023-11-19 16:06:40 +0400

    Add comments for animation

--- simulation/conveyor/animation.py
+++ simulation/conveyor/animation.py
@@ -63,10 +63,12 @@ class Animation:
     def draw_animation(self, filename=None, interval=100):
         fig, ax = plt.subplots()
 
+        # Dict from edges to edges view
         edge_dict = {}
         for edge in self._edges:
             source = None
             target = None
+            # Find corresponding nodes for the edge
             for node in self._nodes:
                 if node["id"] == edge["source"]:
                     source = node
@@ -75,30 +77,40 @@ class Animation:
                 else:
                     continue
             if source is None or target is None:
+                print("Error: edge", edge, "has no corresponding nodes")
                 continue
+            # Draw the edge
             line, = ax.plot([source["a_x"], target["a_x"]], [-source["a_y"], -target["a_y"]], 'k-')
+            # Draw the arrow on the end of the edge
             arrow = FancyArrowPatch((source["a_x"], -source["a_y"]), (target["a_x"], -target["a_y"]), arrowstyle='->',
                                     mutation_scale=20)
             ax.add_patch(arrow)
+            # Save the edge view
             edge_dict[(edge["source"], edge["target"])] = (line, arrow)
 
+        # Dict from nodes to nodes view
         nodes_dict = {}
         for node in self._nodes:
             point, = ax.plot(node["a_x"], -node["a_y"], 'ko', markersize=10)
             nodes_dict[node["id"]] = point
 
-        def get_bag_positions(bag_info):
+        # Get the position of the object on the edge
+        def get_object_position(object_info):
             source = None
             target = None
+            # Find corresponding nodes for the edge on which the object is
             for node in self._nodes:
-                if node["id"] == bag_info.source:
+                if node["id"] == object_info.source:
                     source = node
-                if node["id"] == bag_info.target:
+                if node["id"] == object_info.target:
                     target = node
+            # If the edge is not found, we will not draw the object
+            # on the point (0, 0) with the message in the console
             if source is None or target is None:
+                print("Error: object", object_info, "has no corresponding edge")
                 return {"x": 0, "y": 0}
-            x = source["a_x"] + (target["a_x"] - source["a_x"]) * bag_info.position
-            y = -(source["a_y"] + (target["a_y"] - source["a_y"]) * bag_info.position)
+            x = source["a_x"] + (target["a_x"] - source["a_x"]) * object_info.position
+            y = -(source["a_y"] + (target["a_y"] - source["a_y"]) * object_info.position)
             return {"x": x, "y": y}
 
         objects_dict = {}
@@ -107,38 +119,45 @@ class Animation:
         def update(frame):
             current_objects = {obj.id: obj for obj in self._steps[frame].positions}
 
+            # Redraw objects which are already on the plot
+            # If the object is not in the current step, remove it from the plot
             for o_id, point in list(objects_dict.items()):
                 if o_id in current_objects:
-                    object_position = get_bag_positions(current_objects[o_id])
+                    object_position = get_object_position(current_objects[o_id])
                     point.set_data(object_position["x"], object_position["y"])
                 else:
                     point.remove()
                     del objects_dict[o_id]
 
+            # Draw new objects
             for o_id, obj in current_objects.items():
                 if o_id not in objects_dict:
-                    object_position = get_bag_positions(obj)
+                    object_position = get_object_position(obj)
                     point, = ax.plot(object_position["x"], object_position["y"], 'co')
                     objects_dict[o_id] = point
 
+            # Reset nodes which were colored for one step
             for node in reset_once_nodes:
                 nodes_dict[node].set_color('k')
             reset_once_nodes.clear()
 
             for action in self._steps[frame].actions:
+                # Change the color of the edge
                 if action.type == "edge":
                     nodes = action.id
                     for i in range(len(nodes) - 1):
                         (line, arrow) = edge_dict[(nodes[i], nodes[i + 1])]
                         line.set_color(action.color)
                         arrow.set_color(action.color)
+                # Change the color of the node
+                # If the duration is "once", save the node to reset its color on the next step
                 elif action.type == "node":
                     node = action.id
                     nodes_dict[node].set_color(action.color)
                     if action.duration == "once":
                         reset_once_nodes.append(node)
 
-        # save animation
+        # Save animation
         ani = FuncAnimation(fig, update, frames=len(self._steps), interval=interval, repeat=False)
         if filename is not None:
             ani.save(filename, writer='imagemagick')
